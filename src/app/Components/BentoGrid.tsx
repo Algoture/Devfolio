c365fbd2bc3966ee6d63dfa36216e9c9f8d368f5
Umesh Nagare <umeshnagare27@gmail.com>
2024-12-26 15:51:22 +0530

    bentogrid

---
+++ src/app/Components/BentoGrid.tsx
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import React from "react";
+
+type data = {
+  title: String;
+  value: number;
+  height: number;
+};
+const dataArray: data[] = [
+  { title: "Item One", value: 23, height: 150 },
+  { title: "Item Two", value: 45, height: 200 },
+  { title: "Item Three", value: 12, height: 120 },
+  { title: "Item Four", value: 67, height: 180 },
+  { title: "Item Five", value: 34, height: 160 },
+  { title: "Item Six", value: 78, height: 220 },
+  // { title: "Item Seven", value: 56, height: 190 },
+  // { title: "Item Eight", value: 89, height: 240 },
+];
+const BentoGrid: React.FC = () => {
+  return (
+    <div className="grid grid-cols-3 gap-2 m-10">
+      {dataArray.map(({ title, value }, i) => {
+        return (
+          <div
+            key={i}
+            className={clsx(
+              "bg-accent p-4 text-white bg-orange-400 rounded-xl flex flex-col items-center justify-center",
+              i == 1 ? "col-span-2" : "",
+              i == 4 || i == 2 ? "row-span-2" : ""
+            )}
+          >
+            <h2>{title}</h2>
+            <p>{value}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default BentoGrid
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,37 @@
-import { GitHubIcon } from "./Components/Icons";
-import { aboutMe } from "./utils/data";
+import Image from "next/image";
+import { aboutMe } from "@/app/utils/Data";
+import { buttons } from "./utils/Socials";
+import BentoGrid from "./Components/BentoGrid";
 
 export default function Home() {
   return (
-    <div className="w-full h-dvh center flex-col">
-      {/* <button className="btn">
-        <GitHubIcon width={18} height={18} />
-        GitHub
-      </button> */}
-      <div></div>
-
-      <p className="xl:text-9xl md:text-7xl text-4xl text-left font-semibold">I'm {aboutMe.name}</p>
-      <p className="xl:text-3xl md:text-2xl text-xl text-gray-500 p-3 text-center">
-        A passionate Full-Stack <span className="text-orange-500">&lt;Developer/&gt;</span> with 4+ years of experience.
+    <div className="w-full mt-4 cen flex-col">
+      <Image
+        src="/Joe.jpg"
+        width={200}
+        height={150}
+        className="rounded-full object-cover bg-orange-400 size-52"
+        alt="joe"
+      />
+      <p className="xl:text-9xl md:text-7xl text-4xl text-left font-semibold">
+        I'm {aboutMe.name}
       </p>
+      <p className="xl:text-3xl md:text-2xl text-xl text-gray-500 p-3 text-center">
+        A passionate Full-Stack{" "}
+        <span className="text-orange-400">&lt;Developer/&gt;</span> with 4+
+        years of experience.
+      </p>
+      <div className="cen md:flex-row flex-wrap gap-2 p-2   rounded-lg">
+        {buttons.map(({ name, icon }, i) => {
+          return (
+            <button key={i} className="btn ">
+              {icon}
+              {name}
+            </button>
+          );
+        })}
+      </div>
+      <BentoGrid />
     </div>
   );
 }
