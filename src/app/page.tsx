4b47bd253571a0ac2ea23f421a5782d85af708a6
Umesh Nagare <umeshnagare27@gmail.com>
2024-12-30 18:45:26 +0530

    dark mode integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,56 @@
+"use client";
 import Image from "next/image";
 import { aboutMe } from "@/app/utils/Data";
 import { buttons } from "./utils/Socials";
 import BentoGrid from "./Components/BentoGrid";
-
+import localFont from "next/font/local";
+import { MoonIcon, SunIcon } from "./Components/Icons";
+import { useState } from "react";
+const myFont = localFont({ src: "./font/Louize.woff2" });
 export default function Home() {
+  const [dark, setDark] = useState<boolean>(false);
+  const darkModeHandler = () => {
+    setDark(!dark);
+    document.body.classList.toggle("dark");
+  };
   return (
-    <div className="w-full mt-4 cen flex-col">
-      <Image
-        src="/Joe.jpg"
+    <div className="w-full h-dvh dark:bg-foreground cen flex-col">
+      {/* <Image
+        src="/Joe.png"
         width={200}
         height={150}
-        className="rounded-full object-cover bg-orange-400 size-52"
+        className="rounded object-cover size-52"
         alt="joe"
-      />
+      /> */}
       {/* <Image src="/RoleMaster.png" alt="rolemaster" className="shadow-xl rounded-xl" width={500} height={150}/> */}
-      <p className="xl:text-9xl md:text-7xl text-4xl text-left font-semibold">
+      <p
+        className={`md:text-9xl sm:text-7xl text-5xl txt text-black dark:text-background text-center ${myFont.className}`}
+      >
         I'm {aboutMe.name}
       </p>
-      <p className="xl:text-3xl md:text-2xl text-xl text-gray-500 p-3 text-center">
+      <p className="xl:text-3xl md:text-3xl text-xl dark:text-gray-300 text-gray-500 p-3 text-center">
         A passionate Full-Stack{" "}
-        <span className="text-orange-400">&lt;Developer/&gt;</span> with 4+
+        <span className="text-darkaccent dark:text-accent">&lt;Developer/&gt;</span> with 4+
         years of experience.
       </p>
       <div className="cen md:flex-row flex-wrap gap-2 p-2   rounded-lg">
         {buttons.map(({ name, icon }, i) => {
           return (
-            <button key={i} className="btn ">
+            <button key={i} className="btn">
               {icon}
               {name}
             </button>
           );
         })}
       </div>
-      <BentoGrid />
+      <button
+        onClick={() => darkModeHandler()}
+        className="dark:bg-accent bg-darkaccent cen transition-all dark:text-background absolute right-8 top-8 p-2 rounded-full focus:outline-none"
+      >
+        {dark && <SunIcon width={24} height={24} />}
+        {!dark && <MoonIcon width={24} height={24} />}
+      </button>
+      {/* <BentoGrid /> */}
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        accent: "#F8F32B",
+        darkaccent: "#5928e5",
       },
       boxShadow: {
+        sh2: "0 3px 10px rgb(0,0,0,0.2)",
+        nxt: "0 8px 30px rgb(0,0,0,0.12);",
         sh: "0px 2px 3px -1px rgba(0, 0, 0, .1), 0px 1px 0px 0px rgba(25, 28, 33, .02), 0px 0px 0px 1px rgba(25, 28, 33, .08)",
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;
